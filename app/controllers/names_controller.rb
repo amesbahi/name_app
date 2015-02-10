66267198d136f8cc5024a09ad86cd00c1618e59b
alborz1031 <alborz1031@gmail.com>
2015-02-10 12:57:28 -0500

    Added some views

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -1,8 +1,25 @@
 class NamesController < ApplicationController
+	def index
+		@names = Name.all
+	end
+
 	def new
 	end
 
 	def create
-		render plain: params[:name].inspect
+		@name = Name.new(name_params)
+
+		@name.save
+		redirect_to @name
+	end
+
+	def show
+		@name = Name.find(params[:id])
+	end
+
+	private
+
+	def name_params
+		params.require(:name).permit(:name)
 	end
 end
